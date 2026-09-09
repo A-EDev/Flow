c106498e38d6945a2004984698bab406090274ed
AE <antonioeid40@gmail.com>
2026-09-09 19:29:07 +0300

    refactor(player): split the player gestures into tap and drag files and use the haptic constants directly

--- app/src/main/java/io/github/aedev/flow/ui/components/videoplayer/gesture/PlayerHaptics.kt
+++
@@ -1,8 +0,0 @@
-package io.github.aedev.flow.ui.components.videoplayer.gesture
-
-import androidx.compose.ui.hapticfeedback.HapticFeedback
-import androidx.compose.ui.hapticfeedback.HapticFeedbackType
-
-internal fun HapticFeedback.playerTick() = performHapticFeedback(HapticFeedbackType.TextHandleMove)
-
-internal fun HapticFeedback.playerPress() = performHapticFeedback(HapticFeedbackType.LongPress)
